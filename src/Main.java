373ce1058ffa7212b333c7599fb5246f93e2f924
dspychala22 <spychala.dominika22@gmail.com>
2026-02-24 19:31:08 +0100

    Poprawa pliku testowego

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Witaj Świecie!");
+        System.out.println("Hejka tu Lenka!");
 
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hejka tu Lenka!")
     }
 }
